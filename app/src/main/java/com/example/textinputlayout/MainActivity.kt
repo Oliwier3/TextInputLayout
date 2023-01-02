5603c4799f2ff6db6c047bdaa5e3a4c6c927b421
Jocheu <88211597+Jocheu@users.noreply.github.com>
2023-01-02 14:16:47 +0100

    4 commit

--- app/src/main/java/com/example/textinputlayout/MainActivity.kt
+++ app/src/main/java/com/example/textinputlayout/MainActivity.kt
@@ -4,6 +4,7 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.Button
 import android.widget.TextView
+import com.google.android.material.textfield.TextInputEditText
 import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
@@ -12,8 +13,8 @@ class MainActivity : AppCompatActivity() {
         setContentView(R.layout.activity_main)
 
         findViewById<Button>(R.id.Spr).setOnClickListener {
-            val haslo1 = findViewById<TextInputLayout>(R.id.Haslo)
-            val haslo2 = findViewById<TextInputLayout>(R.id.PHaslo)
+            val haslo1 = findViewById<TextInputEditText>(R.id.Haslo)
+            val haslo2 = findViewById<TextInputEditText>(R.id.PHaslo)
                 if(haslo1 == haslo2)
                     findViewById<TextView>(R.id.odp).text = "Hasła się zgadzają"
                 else
