85cdb92df73f9b35ec78625de0856442365c7a4b
Jocheu <88211597+Jocheu@users.noreply.github.com>
2023-01-02 14:09:21 +0100

    2 commit

--- app/src/main/java/com/example/textinputlayout/MainActivity.kt
+++ app/src/main/java/com/example/textinputlayout/MainActivity.kt
@@ -2,10 +2,20 @@ package com.example.textinputlayout
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import com.google.android.material.textfield.TextInputLayout
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        findViewById<Button>(R.id.Spr).setOnClickListener {
+            val haslo1 = findViewById<TextInputLayout>(R.id.Haslo)
+            val haslo2 = findViewById<TextInputLayout>(R.id.PHaslo)
+                if(haslo1 == haslo2)
+
+        }
+
     }
 }
